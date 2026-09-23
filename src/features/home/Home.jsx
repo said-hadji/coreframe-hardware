e81cebaccc0d5c0f337191133c953f676de0457e
Said Hadji <saidhadji.dev@gmail.com>
2026-09-23 10:41:51 +0000

    fix(home): hero section responsiveness

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -8,15 +8,15 @@ export default function Home() {
   return (
     <div
       className={clsx(
-        "fill-screen bg-[url(/images/heroImg.jpg)] bg-cover bg-center",
+        "relative fill-screen bg-[url(/images/heroImg.jpg)] bg-cover bg-center",
       )}
     >
       {isCart && <div className={clsx("fixed-overlay backdrop-blur-md")} />}
 
-      <div className="absolute top-0 left-0 fill-screen bg-linear-to-r from-zinc-950 via-zinc-950/95 to-zinc-950/30"></div>
+      <div className="absolute top-0 left-0 w-full h-screen bg-linear-to-r from-zinc-950 via-zinc-950/95 to-zinc-950/30"></div>
 
-      <div className="container fill-parent">
-        <div className="absolute z-60 top-1/2 -translate-y-1/2">
+      <div className="container relative fill-parent overflow-hidden">
+        <div className="absolute inset-0 flex-col-justify px-3.5">
           <div>
             <h2 className="text-5xl sm:text-6xl xl:text-8xl font-extrabold font-display text-white">
               Build it right,
@@ -26,12 +26,12 @@ export default function Home() {
             </h1>
           </div>
 
-          <p className="text-zinc-200/70 text-sm sm:text-base xl:text-lg font-sans mt-6">
+          <p className="text-zinc-200/70 text-sm sm:text-base xl:text-lg font-sans pl-0.5 sm:pl-1.5 mt-6">
             Every component, prebuilt PC, and peripheral you need, in stock and
             ready to ship today.
           </p>
 
-          <div className="lg:mt-15 xl:mt-13">
+          <div className="mt-8 lg:mt-9 xl:mt-10 sm:pl-1.5">
             <Button
               type={"discover_new"}
               onClick={() => console.log("clicked")}
